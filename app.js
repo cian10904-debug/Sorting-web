26fcb6cadfddb592f07f52bc58494840c9155f76
I <cian10904@gmail.com>
2026-07-17 01:45:20 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+const t=document.getElementById("t").addEventListener
+const n=document.getElementById("n").addEventListener
+const ad=document.getElementById("ad").addEventListener
+const ads=document.getElementById("ads").addEventListener
+
+function cpl(){
+    const ph=document.getElementById("message");
+    ph.placeholder="Type text to sort here";
+}
+t.addEventListener("click", cpl);
+n.addEventListener("click", function() {
+
+});
+ad.addEventListener("click", function() {
+
+});
+ads.addEventListener("click", function() {
+
+});
